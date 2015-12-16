82306d12c55f590f36572f6a58e2413e9ae93d2c
Daniel Slomovits <daniels220@gmail.com>
2015-12-15 21:03:12 -0500

    Alternate layout—split-and-stacked weekends with a narrow left margin

--- functions.php
+++ functions.php
@@ -1,13 +1,25 @@
 <?php
-function tds($count) {
+function tds($count,$attrs='') {
 	for ($i=0;$i<$count;$i++)
-		echo '<td></td>';
+		echo "<td$attrs></td>";
 }
-function trs($count,$length) {
+function trs($count,$length,$attrs='') {
 	for ($i=0;$i<$count;$i++) {
 		echo '<tr>';
-		tds($length);
+		tds($length,$attrs);
 		echo "</tr>\n";
 	}
+}
+function splitRow() {
+	echo '<tr>';
+	tds(5,' rowspan="2"');
+	tds(1);
+	echo '</tr><tr>';
+	tds(1);
+	echo '</tr>';
+}
+function splitRows($count) {
+	for($i=0;$i<$count;$i++)
+		splitRow();
 }
 ?>
--- month.php
+++ month.php
@@ -1,14 +1,10 @@
 <table class="<?=$classes?>">
 	<tr class="has-spanned-cell">
-		<th class="main-header" rowspan="2">
+		<th class="main-header" rowspan="13"><div>
 			<span class="month-text"><?=$month?></span>
 			<span class="year-text"><?=$year?></span>
-		</th>
-		<th>Monday</th><th>Tuesday</th><th class="wed">Wednesday</th><th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th>
+		</div></th>
+		<th>Monday</th><th>Tuesday</th><th class="wed">Wednesday</th><th>Thursday</th><th>Friday</th><th>Sat. Sun.</th>
 	</tr>
-	<?php trs(1,7); ?>
-	<tr class="has-spanned-cell">
-		<td class="notes-box" rowspan="4">
-		<?php tds(7); ?></tr>
-	<?php trs(3,7); ?>
+	<?php splitRows(5); ?>
 </table>
